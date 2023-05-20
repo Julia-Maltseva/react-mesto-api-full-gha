537fbe672daf60ddf4670bef2588a3ae305242ce
Julia Maltseva <yuliya-maltseva93@yandex.ru>
2023-05-21 00:33:13 +0300

    ПР15

--- backend/utils/urlValidate.js
+++
@@ -1,3 +0,0 @@
-const urlValidate = /https?:\/\/(www\.)?[a-zA-Z0-9\-._~:/?#[\]@!$&'()*+,;=]/;
-
-module.exports = urlValidate;
--- frontend/src/components/ProtectedRoute.js
+++
@@ -1,9 +0,0 @@
-import { Navigate } from "react-router-dom";
-
-function ProtectedRoute({element: Component, ...props}) {
-  return (
-    props.loggedIn ? <Component {...props} /> : <Navigate to='/sign-up' replace />
-  )  
-}
-
-export default ProtectedRoute;
